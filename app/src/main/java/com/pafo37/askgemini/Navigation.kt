02eedb5343369e66d52b21689aa5ef6aba3555e8
Pafo37 <pafo96@mail.bg>
2026-04-13 11:17:43 +0300

    code cleanup

--- app/src/main/java/com/pafo37/askgemini/Navigation.kt
+++
@@ -1,6 +0,0 @@
-package com.pafo37.askgemini
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-object Home
--- app/src/main/java/com/pafo37/askgemini/koin/AppModule.kt
+++ app/src/main/java/com/pafo37/askgemini/koin/AppModule.kt
@@ -6,12 +6,9 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    // 1. Single instance of the Database
     single { AppDatabase.getDatabase(get()) }
 
-    // 2. Single instance of the DAO (gets the DB from above)
     single { get<AppDatabase>().chatDao() }
 
-    // 3. ViewModel factory (gets the DAO from above)
     viewModel { HomeViewModel(get()) }
 }
